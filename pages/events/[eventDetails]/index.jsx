b884941af7e1b0c8f73263358f33789b2a5f4d9d
Luka Chubinidze <chubinidzel08@gmail.com>
2024-01-12 13:00:31 +0400

    practising data rendering different ways

--- pages/events/[eventDetails]/index.jsx
+++ pages/events/[eventDetails]/index.jsx
@@ -2,14 +2,10 @@ import EventContent from "@/components/events/eventContent/event-content";
 import EventLogistics from "@/components/events/eventLogistics/event-logistics";
 import EventSummary from "@/components/events/eventSummary/event-summary";
 import ErrorAlert from "@/components/ui/errorAlert/error-alert";
-import { getEventById } from "@/dummy-data";
-import { useRouter } from "next/router";
+import { getAllEvents, getEventById } from "@/helpers/api-util";
 import React from "react";
 
-export default function EventDetailsPage() {
-  const router = useRouter();
-  const event = getEventById(router.query.eventDetails);
-
+export default function EventDetailsPage({ event }) {
   if (!event) {
     return (
       <ErrorAlert>
@@ -36,4 +32,24 @@ export default function EventDetailsPage() {
       )}
     </>
   );
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventDetails;
+  const event = await getEventById(eventId);
+  return {
+    props: { event },
+  };
+}
+
+export async function getStaticPaths() {
+  const allEvents = await getAllEvents();
+
+  const pathts = allEvents.map((event) => ({
+    params: { eventDetails: event.id },
+  }));
+  return {
+    paths: pathts,
+    fallback: false,
+  };
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,20 @@
 import EventList from "@/components/events/event-list/EventList";
-import { getFeaturedEvents } from "@/dummy-data";
+import { getFeaturedEvents } from "@/helpers/api-util";
 import React from "react";
 
-export default function Home() {
-  const featuredEvents = getFeaturedEvents();
-
+export default function Home({ featuredEvents }) {
   return (
     <div>
       <EventList items={featuredEvents} />
     </div>
   );
+}
+
+export async function getStaticProps(context) {
+  const featuredEvents = await getFeaturedEvents();
+  return {
+    props: {
+      featuredEvents,
+    },
+  };
 }
