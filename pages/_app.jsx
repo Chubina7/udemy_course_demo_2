3ab2c24cc81145a49d6b068c1d312b5eb7b7315c
Luka Chubinidze <chubinidzel08@gmail.com>
2024-01-14 20:25:57 +0400

    learnet and add metadata

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,15 @@
+import Head from "next/head";
 import Layout from "@/components/layout/Layout";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>NextJS events</title>
+        <meta name="description" content="NextJS events" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width"  />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
