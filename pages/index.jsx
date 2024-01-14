a9aef0bbcf176ac16e0815d7da0f7f8064fc8d1e
Luka Chubinidze <chubinidzel08@gmail.com>
2024-01-14 17:18:54 +0400

    add static metadata

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import EventList from "@/components/events/event-list/EventList";
 import { getFeaturedEvents } from "@/helpers/api-util";
 import React from "react";
@@ -5,6 +6,13 @@ import React from "react";
 export default function Home({ featuredEvents }) {
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great new events here that allow you to evolve..."
+        />
+      </Head>
       <EventList items={featuredEvents} />
     </div>
   );
